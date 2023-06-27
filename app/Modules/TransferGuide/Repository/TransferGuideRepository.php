3c383f0e43f6e8e6a555c9ae091035dbc5e919c5
cquispeq <cristhianquispeq@gmail.com>
2023-06-27 12:06:53 -0500

    Ubigeo

--- app/Modules/TransferGuide/Repository/TransferGuideRepository.php
+++ app/Modules/TransferGuide/Repository/TransferGuideRepository.php
@@ -13,72 +13,6 @@ class TransferGuideRepository extends CommonRepository implements ITransferGuide
         try{
             $data = null;
 
-            /* $query = '
-                SELECT 
-                    movimientos_transito.id_movt,
-                    movimientos_transito.action_mov,
-                    movimientos_transito.id_alm_ini,
-                    movimientos_transito.id_alm_des, 
-                    movimientos_transito.observ_mov,
-                    movimientos_transito.nro_mov,
-                    movimientos_transito.fechaguia_mov,
-                    almacen_ini.titulo_alm as almacen_ini_titulo_alm,
-                    almacen_ini.direccion_alm as almacen_ini_direccion_alm,
-                    almacen_des.titulo_alm as almacen_des_titulo_alm,
-                    almacen_des.direccion_alm as almacen_des_direccion_alm,
-                    company_ini.name as company_ini_name,
-                    company_ini.commercial_name as company_ini_commercial_name,
-                    company_ini.document as company_ini_document,
-                    company_des.name as company_des_name,
-                    company_des.commercial_name as company_des_commercial_name,
-                    company_des.document as company_des_document,
-                    document_types_ini.code as document_types_ini_code,
-                    document_types_des.code as document_types_des_code,
-                    document_types_ini.description as document_types_ini_description,
-                    document_types_des.description as document_types_des_description,
-                    u_alm_ini.id_ubigeo as u_alm_ini_id_ubigeo,
-                    u_alm_ini.nombre_ubigeo as u_alm_ini_nombre_ubigeo,
-                    u_alm_ini.codigo_inei as u_alm_ini_codigo_inei,
-                    u_alm_des.id_ubigeo as u_alm_des_id_ubigeo,
-                    u_alm_des.nombre_ubigeo as u_alm_des_nombre_ubigeo,
-                    u_alm_des.codigo_inei as u_alm_des_codigo_inei,
-                    transfers_guides.id as transfers_guides_id,
-                    transfers_guides.serie as transfers_guides_serie,
-                    transfers_guides.number as transfers_guides_number,
-                    transfers_guides.date_issue as transfers_guides_date_issue,
-                    transfers_guides.time_issue as transfers_guides_time_issue,
-                    transfers_guides.observations as transfers_guides_observations,
-                    transfers_guides.motive_code as transfers_guides_motive_code,
-                    transfers_guides.motive_description as transfers_guides_motive_description,
-                    transfers_guides.total_witght as transfers_guides_total_witght,
-                    transfers_guides.unit_measure as transfers_guides_unit_measure,
-                    transfers_guides.email_principal as transfers_guides_email_principal,
-                    transfers_guides.email_secondary as transfers_guides_email_secondary,
-                    transfers_guides.total_quantity as transfers_guides_total_quantity,
-                    transfers_guides.transport_modality as transfers_guides_transport_modality,
-                    transfers_guides.tci_response_code as tci_response_code,
-                    transfers_guides.tci_response_type as tci_response_type,
-                    transfers_guides.tci_response_description as tci_response_description,
-                    transfers_guides.tci_response_date as tci_response_date,
-                    transports.modality as transports_modalidad,
-                    transports.start_date as transports_fecha_inicio,
-                    transports.document_type_code as transports_tipo_documento,
-                    transports.document as transports_documento,
-                    transports.company_name as transports_razon_social,
-                    transports.mtc_number as transports_numero_mtc
-                FROM transfers_guides
-                LEFT JOIN movimientos_transito ON movimientos_transito.id_movt = transfers_guides.movement_id
-                LEFT JOIN almacen as almacen_ini ON almacen_ini.id_alm = movimientos_transito.id_alm_ini
-                LEFT JOIN almacen as almacen_des ON almacen_des.id_alm = movimientos_transito.id_alm_des
-                LEFT JOIN companies as company_ini ON company_ini.id = almacen_ini.company_id
-                LEFT JOIN companies as company_des ON company_des.id = almacen_des.company_id
-                LEFT JOIN document_types as document_types_ini ON document_types_ini.id = company_ini.document_type_id
-                LEFT JOIN document_types as document_types_des ON document_types_des.id = company_des.document_type_id
-                LEFT JOIN ubigeo as u_alm_ini ON u_alm_ini.id_ubigeo = almacen_ini.distrito_alm
-                LEFT JOIN ubigeo as u_alm_des ON u_alm_des.id_ubigeo = almacen_des.distrito_alm
-                LEFT JOIN transports ON transports.transfer_guide_id = transfers_guides.id
-            '; */
-
             $query = '
                     SELECT 
                         transfers_guides.id,
@@ -122,9 +56,13 @@ class TransferGuideRepository extends CommonRepository implements ITransferGuide
                         document_types_des.code as document_types_des_code,
                         document_types_ini.description as document_types_ini_description,
                         document_types_des.description as document_types_des_description,
+                        province_ini.nombre_ubigeo as province_ini_name,
+                        department_ini.nombre_ubigeo as department_ini_name,
                         u_est_ini.id_ubigeo as u_est_ini_id_ubigeo,
                         u_est_ini.nombre_ubigeo as u_est_ini_nombre_ubigeo,
                         u_est_ini.codigo_inei as u_est_ini_codigo_inei,
+                        province_des.nombre_ubigeo as province_des_name,
+                        department_des.nombre_ubigeo as department_des_name,
                         u_est_des.id_ubigeo as u_est_des_id_ubigeo,
                         u_est_des.nombre_ubigeo as u_est_des_nombre_ubigeo,
                         u_est_des.codigo_inei as u_est_des_codigo_inei,
@@ -174,7 +112,11 @@ class TransferGuideRepository extends CommonRepository implements ITransferGuide
                 LEFT JOIN document_types as document_types_ini ON document_types_ini.id = company_ini.document_type_id
                 LEFT JOIN document_types as document_types_des ON document_types_des.id = company_des.document_type_id
                 LEFT JOIN ubigeo as u_est_ini ON u_est_ini.id_ubigeo = establishment_ini.ubigeo_id
+                LEFT JOIN ubigeo as province_ini ON province_ini.id_ubigeo = u_est_ini.id_padre_ubigeo
+                LEFT JOIN ubigeo as department_ini ON department_ini.id_ubigeo = province_ini.id_padre_ubigeo
                 LEFT JOIN ubigeo as u_est_des ON u_est_des.id_ubigeo = establishment_des.ubigeo_id
+                LEFT JOIN ubigeo as province_des ON province_des.id_ubigeo = u_est_des.id_padre_ubigeo
+                LEFT JOIN ubigeo as department_des ON department_des.id_ubigeo = province_des.id_padre_ubigeo
                 LEFT JOIN inventario ON inventario.id_inv = transfer_guide_detail.inventory_id
                 LEFT JOIN transports ON transports.transfer_guide_id = transfers_guides.id
                 LEFT JOIN vehicles ON vehicles.transfer_guide_id = transfers_guides.id
