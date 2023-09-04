2fab0d85f852216519d9ae77e7c8fe351c329cc8
cquispeq <cristhianquispeq@gmail.com>
2023-09-04 09:31:57 -0500

    TEst log

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -12,7 +12,6 @@ call_user_func(array($controller,$action));
 class LoginController {
 
     public function valida_logueoUsuario(){
-        file_put_contents(__DIR__ .'/../logs/log_'.date("j.n.Y").'-'.date("h.i.s").'.log', json_encode("error"), FILE_APPEND);
         try {
             $nroDoc = $_POST['txtNroDoc'];
             $clave = $_POST['txtPassword'];
